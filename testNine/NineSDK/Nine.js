5c8c9d181ca2f29fbe547666a3f40ab97aa0ea15
Yian Zhang <zhangyc@qq.com>
2013-04-19 16:37:48 +0800

    drop all of the version letter

--- testNine/NineSDK/Nine.js
+++ testNine/NineSDK/Nine.js
@@ -2,36 +2,36 @@
 document.writeln("<script src='https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>");
 
 //body
-document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Body_0.1.0.css' />");
+document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Body.css' />");
 
 //menu		
-document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Menu_0.2.5.css' />");
-document.writeln("<script src='NineSDK/MenuItem_0.2.5.js'></script>");
-document.writeln("<script src='NineSDK/MenuBar_0.2.0.js'></script>");
+document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Menu.css' />");
+document.writeln("<script src='NineSDK/MenuItem.js'></script>");
+document.writeln("<script src='NineSDK/MenuBar.js'></script>");
 
 //toolbar
-document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/ToolBar_0.2.5.css' />");
-document.writeln("<script src='NineSDK/ToolItem_0.2.5.js'></script>");
-document.writeln("<script src='NineSDK/ToolBar_0.2.0.js'></script>");
+document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/ToolBar.css' />");
+document.writeln("<script src='NineSDK/ToolItem.js'></script>");
+document.writeln("<script src='NineSDK/ToolBar.js'></script>");
 
 //tree
-document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Tree_0.2.5.css' />");
-document.writeln("<script src='NineSDK/TreeItem_0.2.5.js'></script>");
-document.writeln("<script src='NineSDK/Tree_0.3.0.js'></script>");
+document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Tree.css' />");
+document.writeln("<script src='NineSDK/TreeItem.js'></script>");
+document.writeln("<script src='NineSDK/Tree.js'></script>");
 
 //callboard
-document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/CallBoard_0.1.0.css' />");
-document.writeln("<script src='NineSDK/CallBoard_0.1.0.js'></script>");
+document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/CallBoard.css' />");
+document.writeln("<script src='NineSDK/CallBoard.js'></script>");
 
 //container
-document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Container_0.1.0.css' />");
-document.writeln("<script src='NineSDK/Container_0.1.0.js'></script>");
+document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Container.css' />");
+document.writeln("<script src='NineSDK/Container.js'></script>");
 
 //dialog
-document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Dialog_0.1.0.css' />");
-document.writeln("<script src='NineSDK/Dialog_0.1.0.js'></script>");
+document.writeln("<link rel='stylesheet' type='text/css' href='NineSDK/css/Dialog.css' />");
+document.writeln("<script src='NineSDK/Dialog.js'></script>");
 
 //view
-document.writeln("<script src='NineSDK/view_0.2.0.js'></script>");
+document.writeln("<script src='NineSDK/view.js'></script>");
 
 
